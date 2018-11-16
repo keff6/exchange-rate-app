bca7c0c686880d6fcc6fdb35f7be09d38fb454a0
keff6 <kev.fallas@gmail.com>
2018-11-16 15:05:57 -0600

    Add an endpoint to serve soap response

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const soapRequest = require('easy-soap-request');
+const fs = require('fs');
+var path = require('path');
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
@@ -13,6 +17,33 @@ app.post('/api/world', (req, res) => {
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
+});
+
+
+async function getSoap(url, headers, xml) {
+    try {
+      const { response } = await soapRequest(url, headers, xml);
+      const { body, statusCode } = response;
+      return body;
+      // console.log(statusCode);
+    } catch (error) {
+      console.log(error)
+    }
+} 
+
+app.get('/api/ind', async (req, res) => {
+  const url = 'https://gee.bccr.fi.cr/indicadoreseconomicos/WebServices/wsIndicadoresEconomicos.asmx';
+  const headers = {
+    'user-agent': 'sampleTest',
+    'Content-Type': 'text/xml;charset=UTF-8',
+    'soapAction': 'http://ws.sdde.bccr.fi.cr/ObtenerIndicadoresEconomicos',
+  };
+  const xmlPath = path.join(__dirname, '..', 'server', 'soap', 'economicIndexEnvelope.xml');
+
+  const xml = fs.readFileSync(xmlPath, 'utf-8');
+
+  const resp = await getSoap(url, headers, xml);
+  res.send({ data: resp });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
