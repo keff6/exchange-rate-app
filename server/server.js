c55ec697336feabd9f42204c11e8c8e8a770aa0f
keff6 <kev.fallas@gmail.com>
2018-12-03 18:45:03 -0600

    Server code cleanup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const fs = require('fs');
-var path = require('path');
-const transform = require('camaro')
 const ExchangeService = require('./services/exchange.service');
 
 // Express config
@@ -13,20 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.post('/api/index', async (req, res) => {    
-  const xmlPath = path.join(__dirname, '..', 'server', 'soap', 'economicIndexEnvelope.xml');
-  const xml = fs.readFileSync(xmlPath, 'utf-8');
+app.post('/api/index', async (req, res) => {   
   const exchangeService = new ExchangeService();
-  const result =  exchangeService.setArguments(xml, req.body.startDate, req.body.endDate);
-  const resp = await exchangeService.getSoap(result);
-
-  const template = {
-    data: ['//INGC011_CAT_INDICADORECONOMIC', {
-        date: 'DES_FECHA',
-        value: 'NUM_VALOR'
-    }]
-  }
-  res.send(transform(resp, template));
+  const result = await exchangeService.getDolarIndex(req.body.startDate, req.body.endDate);
+  res.send(result);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- server/services/exchange.service.js
+++ server/services/exchange.service.js
@@ -1,5 +1,8 @@
 
 const soapRequest = require('easy-soap-request');
+const transform = require('camaro')
+const fs = require('fs');
+var path = require('path');
 
 const url = 'https://gee.bccr.fi.cr/indicadoreseconomicos/WebServices/wsIndicadoresEconomicos.asmx';
 const headers = {
@@ -7,10 +10,31 @@ const headers = {
     'Content-Type': 'text/xml;charset=UTF-8',
     'soapAction': 'http://ws.sdde.bccr.fi.cr/ObtenerIndicadoresEconomicos',
 };
+const xmlPath = path.join(__dirname, '../..', 'server', 'soap', 'economicIndexEnvelope.xml');
 
-class ExchangeService {     
-    setArguments(xmlStr, start, end) {
-        const result = xmlStr
+const template = {
+    data: ['//INGC011_CAT_INDICADORECONOMIC', {
+        date: 'DES_FECHA',
+        value: 'NUM_VALOR'
+    }]
+  }
+
+class ExchangeService {   
+    async getDolarIndex(start, end) {
+        try {
+            const xml = await fs.readFileSync(xmlPath, 'utf-8');
+            const envelope = this.setArguments(xml, start, end);
+            console.log(envelope)
+            const soapResult = await this.getSoap(envelope);
+            console.log(soapResult)
+            return transform(soapResult, template);
+        }  catch (error) {
+            console.log('ERR1: '+ error)
+        }
+    }  
+
+    setArguments(xml, start, end) {
+        const result = xml
             .replace('{tcIndicador}', 317)
             .replace('{tcFechaInicio}', start)
             .replace('{tcFechaFinal}', end)
@@ -25,7 +49,7 @@ class ExchangeService {
             const { body, statusCode } = response;
             return body;
         } catch (error) {
-            console.log(error)
+            console.log('ERR2: '+error)
         }
     } 
 }
